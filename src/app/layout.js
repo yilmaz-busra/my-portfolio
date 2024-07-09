9bcc8c75badb17ec1e366f880667d948ce69aed9
busra <b.busraayilmaz@gmail.com>
2024-07-09 23:59:40 +0300

    Added Sidebar component to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../Components/Header";
 import About from "../Components/About";
+import Sidebar from "@/Components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
-        <div className="h-[31.25rem] w-[31.25rem]  sm:w-[68.75rem] absolute -z-10 top-[-6rem] right-[11rem] rounded-full blur-[10rem] bg-[#fbe2e3]">
-          <div className="h-[31.25rem] w-[50rem]  sm:w-[68.75rem] md:left-[33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] absolute -z-10 top-[-1rem] left-[-35rem] rounded-full blur-[10rem] bg-[#fbe2e3]"></div>
+        <div className="h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] absolute -z-10 top-[-6rem] right-[11rem] rounded-full blur-[10rem] bg-[#fbe2e3]">
+          <div className="h-[31.25rem] w-[50rem] sm:w-[68.75rem] md:left-[33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] absolute -z-10 top-[-1rem] left-[-35rem] rounded-full blur-[10rem] bg-[#fbe2e3]"></div>
         </div>
         <Header />
-        {children}
+        <div className="flex flex-col lg:flex-row justify-between max-w-6xl mx-auto">
+          <div className="container sticky top-0 lg:w-1/4">
+            <Sidebar />
+          </div>
+          <div className="lg:w-3/4">{children}</div>
+        </div>
       </body>
     </html>
   );
