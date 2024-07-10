40f117c2a1145614e61a747ceb053592f96c19aa
busra <b.busraayilmaz@gmail.com>
2024-07-11 00:47:53 +0300

    refactor: Update background color and layout in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,12 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
-        <div className="h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] absolute -z-10 top-[-6rem] right-[11rem] rounded-full blur-[10rem] bg-[#fbe2e3]">
+        <div className="h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] absolute -z-10 top-[-6rem] right-[11rem] rounded-full blur-[10rem] bg-[#D9D9D9]">
           <div className="h-[31.25rem] w-[50rem] sm:w-[68.75rem] md:left-[33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] absolute -z-10 top-[-1rem] left-[-35rem] rounded-full blur-[10rem] bg-[#fbe2e3]"></div>
         </div>
         <Header />
         <div className="flex flex-col lg:flex-row justify-between max-w-6xl mx-auto">
-          <div className="container sticky top-0 lg:w-1/4">
+          <div className="min-h-screen flex items-center justify-center ">
             <Sidebar />
           </div>
           <div className="lg:w-3/4">{children}</div>
