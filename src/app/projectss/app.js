f7a20607b55dd8428ffb800649136c09d0003393
busra <b.busraayilmaz@gmail.com>
2024-07-16 23:35:22 +0300

    feat: Added Projects page component

--- src/app/projectss/app.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function app() {
-  return <div>app</div>;
-}
-
-export default app;
---
+++ src/app/projectss/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Projectss() {
+  return (
+    <div className="px-3 py-12 mx-auto  rounded-xl shadow-xl overflow-hidden md:max-w-2xl bg-white">
+      <h1 className="">Projects</h1>
+      <p>Here are some of the projects I have worked on:</p>
+      <ul>
+        <li></li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projectss;
