31801774527c6b82ddaca03d6918b657896fcae7
busra <b.busraayilmaz@gmail.com>
2024-08-02 20:51:37 +0300

    refactor: Update import paths for Sidebar and Header components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "../components/Sidebar";
 
-import Header from "@/components/Header";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
